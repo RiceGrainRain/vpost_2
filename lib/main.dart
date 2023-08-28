a5bcf601f49e44bde15399e10e2c72a68b856632
RiceGrainRain <mnmanasnavale@gmail.com>
2023-08-27 20:36:21 -0500

    added images and finalizing firebase;

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 //vpost-2
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vpost_2/responses/mobile_screen_layout.dart';
 import 'package:vpost_2/responses/responsive_layout.dart';
 import 'package:vpost_2/responses/web_layout.dart';
 import 'package:vpost_2/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -20,12 +23,11 @@ class MyApp extends StatelessWidget {
       title: 'VPOST-2',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
-
       ),
       home: const ResponsiveLayout(
-        webScreenLayout: WebScreenLayout(),
-        mobileScreenLayout: MobileScreenLayout()
-        ),
+          webScreenLayout: WebScreenLayout(),
+          mobileScreenLayout: MobileScreenLayout(),
+          ),
     );
   }
 }
