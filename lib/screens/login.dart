c2c9414978127b596d63862c1da5636e5676f587
RiceGrainRain <mnmanasnavale@gmail.com>
2023-08-28 08:14:55 -0500

    added ios config; and attempting scsv

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -22,88 +22,92 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        width: double.infinity,
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          Flexible(flex: 2, child: Container()),
-          const SizedBox(
-            height: 20,
-          ),
-          //LOGO
-          Image.asset(
-            'assets/images/vpost-dark.png',
-            height: 180,
-            width: 180,
-          ),
-          //email
-          TextFieldInput(
-              textEditingController: _emailController,
-              hintText: "Email",
-              textInputType: TextInputType.emailAddress),
-
-          const SizedBox(
-            height: 20,
-          ),
-
-          //password
-          TextFieldInput(
-            textEditingController: _passwordController,
-            hintText: "Password",
-            textInputType: TextInputType.text,
-            isPass: true,
-          ),
-
-          const SizedBox(
-            height: 20,
-          ),
-
-          //Login
-          InkWell(
-            child: Container(
-              alignment: Alignment.center,
-              width: double.infinity,
-              padding: const EdgeInsets.symmetric(vertical: 22),
-              decoration: const ShapeDecoration(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(4))),
-                color: greenColor,
+    return Expanded(
+      child: SingleChildScrollView(
+        child: Scaffold(
+          body: SafeArea(
+              child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            width: double.infinity,
+            child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+              Flexible(flex: 2, child: Container()),
+              const SizedBox(
+                height: 20,
               ),
-              child: const Text(
-                "Log in",
-                style: TextStyle(fontWeight: FontWeight.bold),
+              //LOGO
+              Image.asset(
+                'assets/images/vpost-dark.png',
+                height: 180,
+                width: 180,
               ),
-            ),
-          ),
-
-          const SizedBox(
-            height: 12,
-          ),
-          Flexible(flex: 2, child: Container()),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: const Text("Don't have an account?"),
+              //email
+              TextFieldInput(
+                  textEditingController: _emailController,
+                  hintText: "Email",
+                  textInputType: TextInputType.emailAddress),
+          
+              const SizedBox(
+                height: 20,
               ),
+          
+              //password
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: "Password",
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+          
+              const SizedBox(
+                height: 20,
+              ),
+          
+              //Login
               InkWell(
                 child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  alignment: Alignment.center,
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 22),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(4))),
+                    color: greenColor,
+                  ),
                   child: const Text(
-                    "Sign Up",
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, color: blueColor),
+                    "Log in",
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
-              )
-            ],
-          ),
-        ]),
-      )),
+              ),
+          
+              const SizedBox(
+                height: 12,
+              ),
+              Flexible(flex: 2, child: Container()),
+          
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: const Text("Don't have an account?"),
+                  ),
+                  InkWell(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: const Text(
+                        "Sign Up",
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, color: blueColor),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ]),
+          )),
+        ),
+      ),
     );
   }
 }
