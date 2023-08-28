9807753e2e0e51369322410e0971af07bf18d340
RiceGrainRain <mnmanasnavale@gmail.com>
2023-08-27 22:15:25 -0500

    finished login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:vpost_2/utils/colors.dart';
+import 'package:vpost_2/widgets/text_field.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,10 +10,100 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("This is the login page"),
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        width: double.infinity,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Flexible(flex: 2, child: Container()),
+          const SizedBox(
+            height: 20,
+          ),
+          //LOGO
+          Image.asset(
+            'assets/images/vpost-dark.png',
+            height: 180,
+            width: 180,
+          ),
+          //email
+          TextFieldInput(
+              textEditingController: _emailController,
+              hintText: "Email",
+              textInputType: TextInputType.emailAddress),
+
+          const SizedBox(
+            height: 20,
+          ),
+
+          //password
+          TextFieldInput(
+            textEditingController: _passwordController,
+            hintText: "Password",
+            textInputType: TextInputType.text,
+            isPass: true,
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+
+          //Login
+          InkWell(
+            child: Container(
+              alignment: Alignment.center,
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 22),
+              decoration: const ShapeDecoration(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(4))),
+                color: greenColor,
+              ),
+              child: const Text(
+                "Log in",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            height: 12,
+          ),
+          Flexible(flex: 2, child: Container()),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: const Text("Don't have an account?"),
+              ),
+              InkWell(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: const Text(
+                    "Sign Up",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, color: blueColor),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ]),
+      )),
     );
   }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,5 +4,6 @@ const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
 const blueColor = Color.fromRGBO(0, 149, 246, 1);
+const greenColor = Color.fromARGB(255, 0, 193, 99);
 const primaryColor = Colors.white;
 const secondaryColor = Colors.grey;
