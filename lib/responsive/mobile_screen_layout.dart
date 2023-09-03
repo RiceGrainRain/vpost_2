9b4cb6803cc18e7b6a7292ab84c4864431f58935
RiceGrainRain <mnmanasnavale@gmail.com>
2023-09-03 16:06:37 -0500

    can call userinfo using providers

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vpost_2/models/user.dart' as model;
+import 'package:vpost_2/providers/user_provider.dart';
+
 
 
 class MobileScreenLayout extends StatefulWidget {
@@ -11,24 +13,11 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
-  String username = '';
-
-  @override void initState() {
-    super.initState();
-    getUserName();
-  }
-
-  void getUserName() async {
-    DocumentSnapshot snap = await FirebaseFirestore.instance.collection('users').doc(FirebaseAuth.instance.currentUser!.uid).get();
-    
-    setState(() {
-      username = (snap.data() as Map<String, dynamic>)['displayName'];
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("mobile layout"),)
+    model.User user = Provider.of<UserProvider>(context).getUser;
+    return Scaffold(
+      body: Center(child: Text(user.displayName),)
     );
   }
 }
