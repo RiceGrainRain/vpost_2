b50bc2c0be21c716f11fcdc3925ec58636330f1d
RiceGrainRain <mnmanasnavale@gmail.com>
2023-09-03 18:53:09 -0500

    working image selection.

--- lib/screens/add_post.dart
+++ lib/screens/add_post.dart
@@ -1,5 +1,11 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:vpost_2/models/user.dart';
+import 'package:vpost_2/providers/user_provider.dart';
 import 'package:vpost_2/utils/colors.dart';
+import 'package:vpost_2/utils/utils.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -9,75 +15,111 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  Uint8List? _file;
+
+  _selectImage(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: const Text('Create a Post'),
+            children: [
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Take a photo'),
+                onPressed: () async {
+                  Uint8List file = await pickImage(ImageSource.camera);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Choose from Gallery'),
+                onPressed: () async {
+                  Uint8List file = await pickImage(ImageSource.gallery);
+                  setState(() {
+                    _file = file;
+                  });
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-/*      return  Center(
-      child: IconButton(
-         icon: const Icon(Icons.upload),
-         onPressed: () {
+    final User user = Provider.of<UserProvider>(context).getUser;
 
-        },
-        )
-     ); */
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {},
-        ),
-        title: const Text('create post'),
-        centerTitle: false,
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Post',
-              style: TextStyle(
-                  color: greenColor, fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-          ),
-        ],
-      ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const CircleAvatar(
-                backgroundImage: NetworkImage("https://i.stack.imgur.com/l60Hf.png"),
+    return _file == null
+        ? Center(
+            child: IconButton(
+            icon: const Icon(Icons.upload),
+            onPressed: () => _selectImage(context),
+          ))
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {},
               ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width*0.3,
-                child: const TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Give a description...',
-                    border: InputBorder.none,
+              title: const Text('Make a new post!'),
+              centerTitle: false,
+              actions: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Post',
+                    style: TextStyle(
+                        color: greenColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
                   ),
-                  maxLines: 8,
                 ),
-              ),
-              SizedBox(
-                height: 45,
-                width: 45,
-                child: AspectRatio(
-                  aspectRatio: 487/51,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(image: NetworkImage("https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"),
-                        fit: BoxFit.fill,
-                        alignment: FractionalOffset.center,
+              ],
+            ),
+            body: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(user.photoUrl),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.3,
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Give a description...',
+                          border: InputBorder.none,
+                        ),
+                        maxLines: 8,
                       ),
                     ),
-                  ),
-                  ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
+                    SizedBox(
+                      height: 45,
+                      width: 45,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 51,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: MemoryImage(_file!),
+                              fit: BoxFit.fill,
+                              alignment: FractionalOffset.center,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
   }
 }
