d64221864883b5692920251928c2fc17abbc7278
RiceGrainRain <mnmanasnavale@gmail.com>
2023-09-05 15:22:22 -0500

    error on Listview; almost done

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,26 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vpost_2/utils/colors.dart';
 import 'package:vpost_2/widgets/post_card.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../utils/dimensions.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
+
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
         centerTitle: false,
-        title: const Text('Today\'s opportunities', style: TextStyle(color: greenColor),),
+        title: const Text(
+          'Today\'s opportunities',
+          style: TextStyle(color: greenColor),
+        ),
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.info_circle)),
+          IconButton(
+              onPressed: () {}, icon: const Icon(CupertinoIcons.info_circle)),
         ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(15.0),
-        child: PostCard(),
+        body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (ctx, index) => Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: width > webScreenSize ? width * 0.3 : 0,
+                vertical: width > webScreenSize ? 15 : 0,
+              ),
+              child: PostCard(
+                snap: snapshot.data!.docs[index].data(),
+              ),
+            ),
+          );
+        },
       ),
     );
-  }
+}
 }
--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:vpost_2/utils/colors.dart';
 
 class PostCard extends StatelessWidget {
-  const PostCard({super.key});
+  final snap;
+  const PostCard({super.key, required this.snap});
 
   @override
   Widget build(BuildContext context) {
@@ -22,26 +23,25 @@ class PostCard extends StatelessWidget {
             ).copyWith(right: 0),
             child: Row(
               children: [
-                const CircleAvatar(
+                CircleAvatar(
                   radius: 16,
-                  backgroundImage: NetworkImage(
-                      'https://media.istockphoto.com/id/1300512215/photo/headshot-portrait-of-smiling-ethnic-businessman-in-office.jpg?s=612x612&w=0&k=20&c=QjebAlXBgee05B3rcLDAtOaMtmdLjtZ5Yg9IJoiy-VY='),
+                  backgroundImage: NetworkImage(snap['profImage']),
                 ),
-                const Expanded(
+                Expanded(
                   child: Padding(
-                    padding: EdgeInsets.only(left: 8),
+                    padding: const EdgeInsets.only(left: 8),
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          'title',
-                          style: TextStyle(
+                          snap['title'],
+                          style: const TextStyle(
                               fontWeight: FontWeight.bold, fontSize: 18),
                         ),
                         Text(
-                          'By username • 11/19/2005',
-                          style: TextStyle(color: secondaryColor),
+                          'By ${snap['username']} • ${snap['datePublished']}',
+                          style: const TextStyle(color: secondaryColor),
                         ),
                       ],
                     ),
@@ -83,26 +83,25 @@ class PostCard extends StatelessWidget {
             height: MediaQuery.of(context).size.height * 0.35,
             width: double.infinity,
             child: Image.network(
-                'https://engineering.fb.com/wp-content/uploads/2022/08/IG-constrained-notification-system-image-1.png',
+                snap['postUrl'],
                 fit: BoxFit.cover),
           ),
 
-          SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Container(
+
+        Container(
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    'Deserunt et eu elit enim aliquip quis duis adipisicing. Laboris adipisicing exercitation laborum ea. Incididunt do eiusmod mollit ea et laborum sit labore enim pariatur ipsum.',
+                    snap['description'],
                     style: Theme.of(context).textTheme.bodyMedium,
                   )
                 ],
               ),
             ),
-          ),
+
 
           //bookmark, share
           Expanded(
@@ -123,12 +122,16 @@ class PostCard extends StatelessWidget {
                     onPressed: () {},
                     icon: const Icon(CupertinoIcons.share),
                   ),
-                  const Expanded(
-                    child: Align(
+                  Expanded(
+                      child: Align(
                     alignment: Alignment.bottomRight,
                     child: Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Text('At Location', style: TextStyle(color: secondaryColor),),),
+                      padding: const EdgeInsets.all(16),
+                      child: Text(
+                        snap['location'],
+                        style: const TextStyle(color: secondaryColor),
+                      ),
+                    ),
                   ))
                 ],
               ),
