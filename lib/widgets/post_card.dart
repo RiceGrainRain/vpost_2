6fafc9e61cdd76eba6a00d90be8262fbb30a8f16
RiceGrainRain <mnmanasnavale@gmail.com>
2023-09-11 23:05:20 -0500

    changed size of post.

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -12,7 +12,7 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 550,
+      height: 500,
       decoration: BoxDecoration(
           border: Border.all(color: Colors.white),
           color: mobileBackgroundColor,
