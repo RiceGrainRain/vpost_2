5a4ee70e06321206c9c37b95bdf4f3bf894698a1
RiceGrainRain <mnmanasnavale@gmail.com>
2023-10-01 15:34:02 -0500

    tabbar to user section

--- lib/screens/user_profile.dart
+++ lib/screens/user_profile.dart
@@ -1,98 +1,135 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vpost_2/utils/colors.dart';
 import 'package:vpost_2/widgets/edit_profile_button.dart';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-class _ProfileScreenState extends State<ProfileScreen> {
+class _ProfileScreenState extends State<ProfileScreen> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: mobileBackgroundColor,
-          title: Text('Your Profile'),
-          centerTitle: false,
-        ),
-        body: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      const CircleAvatar(
-                        backgroundColor: Colors.grey,
-                        backgroundImage: NetworkImage(
-                            'https://plus.unsplash.com/premium_photo-1669638780803-ce74f7f3ea76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80'),
-                        radius: 40,
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisSize: MainAxisSize.max,
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                buildStatColumn(20, "Posts"),
-                                buildStatColumn(40, "Bookmarks"),
-                                buildStatColumn(100, "hours")
-                              ],
-                            ),
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: Text('Your Profile'),
+        centerTitle: false,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      backgroundImage: NetworkImage(
+                          'https://plus.unsplash.com/premium_photo-1669638780803-ce74f7f3ea76?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80'),
+                      radius: 40,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              buildStatColumn(20, "Posts"),
+                              buildStatColumn(40, "Bookmarks"),
+                              buildStatColumn(100, "hours"),
+                            ],
+                          ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
                               FollowButton(
                                 backgroundColor: mobileBackgroundColor,
-                                borderColor:   Colors.grey, 
-                                text: 'Edit Profile', 
+                                borderColor: Colors.grey,
+                                text: 'Edit Profile',
                                 textColor: primaryColor,
                                 function: () {},
-                                )
+                              ),
                             ],
                           ),
                         ],
                       ),
-                      ),
-                    ],
-                  ),
-                  
-                ],
-              ),
-            )
-          ],
-        ));
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          // TabBar
+          TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(icon: Icon(CupertinoIcons.bookmark_fill),),
+              Tab(icon: Icon(CupertinoIcons.graph_square)),
+              Tab(icon: Icon(CupertinoIcons.settings)),
+            ],
+          ),
+          // TabBarView
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                // Content for Section 1
+                Center(child: Text('Section 1 Content')),
+                // Content for Section 2
+                Center(child: Text('Section 2 Content')),
+                // Content for Section 3
+                Center(child: Text('Section 3 Content')),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  Column buildStatColumn(int num, String label,) {
+  Column buildStatColumn(int num, String label) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       mainAxisSize: MainAxisSize.min,
       children: [
-        Text(num.toString(),
-        style: const TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold, 
-        ),
+        Text(
+          num.toString(),
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         Container(
           margin: EdgeInsets.only(top: 4),
           child: Text(
             label,
             style: const TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w400,
-              color: Colors.grey
-            ),
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
           ),
         ),
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the TabController when not needed to prevent memory leaks.
+    _tabController.dispose();
+    super.dispose();
   }
 }
