dd9a711abde5d6dd8db69cf98352c698704a76ca
RiceGrainRain <mnmanasnavale@gmail.com>
2023-10-01 21:30:04 -0500

    post disappears based on whether it is bookmarked or not

--- lib/widgets/bookmark_section.dart
+++ lib/widgets/bookmark_section.dart
@@ -1,10 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vpost_2/models/user.dart' as model;
 import 'package:vpost_2/widgets/bookmark_card.dart';
+import '../providers/user_provider.dart';
 
-class BookmarkSection extends StatelessWidget {
+class BookmarkSection extends StatefulWidget {
+  @override
+  State<BookmarkSection> createState() => _BookmarkSectionState();
+}
+
+class _BookmarkSectionState extends State<BookmarkSection> {
   @override
   Widget build(BuildContext context) {
+    final model.User user = Provider.of<UserProvider>(context).getUser;
     return StreamBuilder(
       stream: FirebaseFirestore.instance.collection('posts').snapshots(),
       builder: (context,
@@ -15,7 +24,12 @@ class BookmarkSection extends StatelessWidget {
           );
         }
 
-        final List<DocumentSnapshot> bookmarkedPosts = snapshot.data!.docs;
+        final List<DocumentSnapshot> posts = snapshot.data!.docs;
+
+        final List<DocumentSnapshot> bookmarkedPosts = posts.where((doc) {
+          final List<dynamic> bookmarks = doc['bookmarks'] ?? [];
+          return bookmarks.contains(user.uid);
+        }).toList();
 
         return ListView.builder(
           itemCount: bookmarkedPosts.length,
