a7f998f7259c9bd10a25ed78f4697304f7cf4dc9
RiceGrainRain <mnmanasnavale@gmail.com>
2023-10-11 17:29:45 -0500

    location search does not work without blaze plan on firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 //vpost-2
+//requires blaze plan for google maps locaition search. 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
